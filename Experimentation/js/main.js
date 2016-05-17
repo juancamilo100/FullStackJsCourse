3f33e62edf876fb853b438e056655efde15cc86e
JuanEspinosa <juan.espinosa2@ge.com>
2016-05-17 18:34:08 -0400

    Doing the first challenge of week #3

--- Experimentation/js/main.js
+++ Experimentation/js/main.js
@@ -1,1 +1,20 @@
-$(".animsition").animsition();
+var array = [1,2,3,4,5];
+var sum = 0;
+
+for(i in array)
+{
+  sum += array[i];
+}
+
+console.log("The sum is: ", sum);
+
+array.splice(2, 0, 6);
+
+for(i in array)
+{
+  console.log(array[i]);
+}
+
+printList(array);
+
+$(".main-content").
